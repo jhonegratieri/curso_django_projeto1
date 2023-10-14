08b97fcf9290fac381e0f62752686f20d0fc3826
jhonegratieri <jhonatansimoni@gmail.com>
2023-10-14 19:04:59 -0300

    create first test and pytest.ini

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class RecipeURLsTest(TestCase):
+
+    def test_the_pytest_is_ok(self):
+        self.assertTrue(True)
+        # variavel = '123456'
+        print('HELLO WORLD')
+        assert 1 == 1, 'One is equal to one'
