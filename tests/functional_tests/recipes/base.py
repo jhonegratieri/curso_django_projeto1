911c7a9369eed19cad2026ec94b47d980c4719ef
jhonegratieri <jhonatansimoni@gmail.com>
2023-12-17 13:02:27 -0300

    create tests with selenium

---
+++ tests/functional_tests/recipes/base.py
@@ -0,0 +1,20 @@
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from utils.browser import make_chrome_browser
+
+# from django.test import LiveServerTestCase
+
+
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
+    def sleep(self, seconds=5):
+        time.sleep(seconds)
