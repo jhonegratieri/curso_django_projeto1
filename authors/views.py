bd7106b358fa2646f646711d54d2c794a9d23d1f
jhonegratieri <jhonatansimoni@gmail.com>
2023-12-29 16:13:26 -0300

    saves recipe edits in the database

--- authors/views.py
+++ authors/views.py
@@ -117,12 +117,25 @@ def dashboard_recipe_edit(request, id):
     if not recipe:
         raise Http404()
 
-    form = AuthorRecipeForm(data=request.POST or None, instance=recipe)
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        files=request.FILES or None,
+        instance=recipe,
+    )
+
+    if form.is_valid():
+        # Agora, o form é válido e eu posso tentar salvar
+        recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, "Sua receita foi salva com sucesso!")
+        return redirect(reverse("authors:dashboard_recipe_edit", args=(id,)))
 
     return render(
-        request,
-        "authors/pages/dashboard_recipe.html",
-        context={
-            "form": form,
-        },
+        request, "authors/pages/dashboard_recipe.html", context={"form": form}
     )
