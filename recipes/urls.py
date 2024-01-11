d4d58984033d4cbc79b977c47091af51e0572e9d
jhonegratieri <jhonatansimoni@gmail.com>
2024-01-11 10:58:15 -0300

    JSONResponse in ListView and DetailView

--- recipes/urls.py
+++ recipes/urls.py
@@ -25,4 +25,14 @@ urlpatterns = [
         views.RecipeDetail.as_view(),
         name="recipe",
     ),
+    path(
+        "recipes/api/v1/",
+        views.RecipeListViewHomeApi.as_view(),
+        name="recipes_api_v1",
+    ),
+    path(
+        "recipes/api/v1/<int:pk>/",
+        views.RecipeDetailAPI.as_view(),
+        name="recipes_api_v1_detail",
+    ),
 ]
